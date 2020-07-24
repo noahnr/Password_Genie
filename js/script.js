67c19380f415540664721db816da7ed9da51bfa0
Journey Cruz <journey.cruz@gmail.com>
2020-07-23 21:17:53 -0500

    Meet the team owl carousel functions for timouts and transition speed

--- js/script.js
+++ js/script.js
@@ -21,16 +21,39 @@ $('.navbar-toggler').click(function () { //when navbar-toggler is clicked
 
 /*========== COLLAPSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK ==========*/
 $(document).on('click', 'a[href^="#"]', function (event) { //when link with "#" clicked
-  event.preventDefault(); //prevent default click event
-  $('.navbar-toggler').addClass('collapsed'); //add collapsed class to toggler
-  $('#navbarResponsive').removeClass('show'); //remove class show from navbar
+    event.preventDefault(); //prevent default click event
+    $('.navbar-toggler').addClass('collapsed'); //add collapsed class to toggler
+    $('#navbarResponsive').removeClass('show'); //remove class show from navbar
 
-  setTimeout(function () { //delay execution of the following    
-    $('.navbar').removeClass('solid-toggle'); //remove class solid-toggle from navbar     
-  }, 300); //delay 300ms
+    setTimeout(function () { //delay execution of the following    
+        $('.navbar').removeClass('solid-toggle'); //remove class solid-toggle from navbar     
+    }, 300); //delay 300ms
 
-  $('html, body').animate({ //animate window scrolling (on click of "#" link)
-      scrollTop: $($.attr(this, 'href')).offset().top //when scrolling to link destination
-  }, 1000); //at animated window speed of 1000ms
+    $('html, body').animate({ //animate window scrolling (on click of "#" link)
+        scrollTop: $($.attr(this, 'href')).offset().top //when scrolling to link destination
+    }, 1000); //at animated window speed of 1000ms
 });
 
+
+/*========== MEET THE TEAM CAROUSEL ==========*/
+$(document).ready(function () { //when document(DOM) loads completely
+    $('#team-carousel').owlCarousel({ //owlCarousel settings
+        autoplay: true, //set to false to turn off autoplay and only use nav
+        autoplayHoverPause: true, //set to false to prevent pausing on hover
+        loop: true, //set to false to stop carousel after all slides shown
+        autoplayTimeout: 8000, //time between transitions
+        smartSpeed: 1200, //transition speed
+        dotsSpeed: 1000, //transition speed when using dots/buttons
+        responsive: { //set number of items shown per screen width
+            0: {
+                items: 1 //0px width and up display 1 item
+            },
+            768: {
+                items: 2 //768px width and up display 2 items
+            },
+            992: {
+                items: 3 //992px width and up display 3 items
+            }
+        }
+    });
+});
