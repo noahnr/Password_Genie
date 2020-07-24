d4ff414c80b12d7ee060c489861cfc00347304df
Journey Cruz <journey.cruz@gmail.com>
2020-07-23 21:14:52 -0500

    add function for navbar color transition on scroll

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*========== NAVBAR TRANSPARENT TO SOLID ==========*/
+$(document).ready(function () { //when document(DOM) loads completely
+    checkScroll(); //check if page is scrolled
+    $(window).scroll(checkScroll); //get scroll position of window
+  });
+  
+  function checkScroll() { //check if page is scrolled
+    if ($(window).scrollTop() >= 300) { //if window is scrolled 300px or more
+        $('.navbar').addClass('solid'); //add class 'solid' to element with class 'navbar'
+    } else { //if page is not scrolled 300px from top
+        $('.navbar').removeClass('solid'); //remove class 'solid' from navbar element
+    }
+  }
+  
+  
