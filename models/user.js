07b27fc9758e5a1d81db5b10a16060aac9184c56
Noah Soukup <noahsoukupraesch@gmail.com>
2020-07-25 09:59:19 -0500

    added a rule that the users email cannot be null

---
+++ models/user.js
@@ -0,0 +1,14 @@
+// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
+var bcrypt = require("bcryptjs");
+// Creating our User model
+module.exports = function (sequelize, DataTypes) {
+        var User = sequelize.define("User", {
+                    // The email cannot be null, and must be a proper email before creation
+                    email: {
+                        type: DataTypes.STRING,
+                        allowNull: false,
+                        unique: true,
+                        validate: {
+                            isEmail: true
+                        }
+                    },
