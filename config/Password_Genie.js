2f4809e17c82101a699308ab91cb8eb1fe012376
Noah Soukup <noahsoukupraesch@gmail.com>
2020-07-25 13:14:20 -0500

    changes to use sequelize

--- config/Password_Genie.js
+++ config/Password_Genie.js
@@ -1,5 +1,5 @@
-var Password_Genie = require("Password_Genie");
-var LocalStrategy = require("Password_Genie-local").Strategy;
+var Password_Genie = require("password_genie");
+var LocalStrategy = require("password_genie-local").Strategy;
 
 var db = require("../models");
 
