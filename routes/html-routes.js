66a16c34555449ba817d62b9acf347efc7cb4074
Noah Soukup <noahsoukupraesch@gmail.com>
2020-07-28 19:41:07 -0500

    tags added

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
         if (req.user) {
             res.redirect("/login");
         }
-        res.render(path.join(__dirname, "../public/members.html"));
+        res.render(path.join(__dirname, "index"));
     });
 
     // Here we've add our isAuthenticated middleware to this route.
