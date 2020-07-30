33761337c0167631df2af5dbcf386bc9cc4b97ff
Journey Cruz <journey.cruz@gmail.com>
2020-07-29 19:20:41 -0500

    updated navbar

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () { //when document(DOM) loads completely
 });
 
 function checkScroll() { //check if page is scrolled
-    if ($(window).scrollTop() >= 300) { //if window is scrolled 300px or more
+    if ($(window).scrollTop() >= 10) { //if window is scrolled 300px or more
         $('.navbar').addClass('solid'); //add class 'solid' to element with class 'navbar'
     } else { //if page is not scrolled 300px from top
         $('.navbar').removeClass('solid'); //remove class 'solid' from navbar element
