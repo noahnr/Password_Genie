c033853d6619d6638f96a2ce9b3884da4d606377
Journey Cruz <journey.cruz@gmail.com>
2020-07-30 21:27:35 -0500

    attempting to create a new model named site

--- models/user.js
+++ models/user.js
@@ -3,65 +3,52 @@ var bcrypt = require("bcryptjs");
 // Creating our User model
 module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define("User", {
-        // The email cannot be null, and must be a proper email before creation
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true
+            // The email cannot be null, and must be a proper email before creation
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isEmail: true
+                }
+            },
+            // The password cannot be null
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false
             }
-        },
-        // The password cannot be null
-        password: {
+    }
+    
+    var Site = sequelize.define("Site", {
+        site: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: true,
+            unique: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: false,
+        },
+        sPassword: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: false,
         }
     });
-
-    module.exports = function (sequelize, Datatypes) {
-        var Site = sequelize.define("Site", {
-            site: {
-                type: DataTypes.STRING,
-                allowNull: true,
-                unique: false,
-            },
-            username: {
-                type: DataTypes.STRING,
-                allowNull: true,
-                unique: false,
-            },
-            sPassword: {
-                type: DataTypes.STRING,
-                allowNull: true,
-                unique: false,
-            }
-        })
-    }
-    // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-    User.prototype.validPassword = function (password) {
-        return bcrypt.compareSync(password, this.password);
-    };
-    // Hooks are automatic methods that run during various phases of the User Model lifecycle
-    // In this case, before a User is created, we will automatically hash their password
-    User.addHook("beforeCreate", function (site) {
-        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-    });
-    return User;
-
-    // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-    Site.prototype.validPassword = function (password) {
-        return bcrypt.compareSync(password, this.password);
-    };
-    // Hooks are automatic methods that run during various phases of the User Model lifecycle
-    // In this case, before a User is created, we will automatically hash their password
-    Site.addHook("beforeCreate", function (site) {
-        user.password = bcrypt.hashSync(site.password, bcrypt.genSaltSync(10), null);
-    });
-    return Site;
+}
+// Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
 };
+// Hooks are automatic methods that run during various phases of the User Model lifecycle
+// In this case, before a User is created, we will automatically hash their password
+User.addHook("beforeCreate", function (site) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+});
+return User;
