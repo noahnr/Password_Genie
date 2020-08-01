b690bbd8f811395eaed1a52da9dd5b2e8c34c5c1
Noah Soukup <noahsoukupraesch@gmail.com>
2020-07-31 19:49:52 -0500

    got a little further

--- public/js/members.js
+++ public/js/members.js
@@ -15,7 +15,7 @@ $(document).ready(() => {
     if (!newSite.site || !newSite.username || !newSite.sPassword) {
       return;
     }
-    
+
     saveNewSite(newSite.site, newSite.username, newSite.sPassword);
     siteName.val("");
     siteUname.val("");
@@ -26,16 +26,36 @@ $(document).ready(() => {
     $.post("/api/new_site_data", {
       site: site,
       username: username,
-      sPassword, sPassword
-    })
-      .then((data) => {
-        $("#username").val(data.username);
-    })
+      sPassword: sPassword
+
+    }).then(data => {
+      $.get("/api/new_site_data").then(data => {
+        for (var i = 0; i < data.length; i++) {
+          // create a parent div for the oncoming elements
+          var siteData = $("<div>");
+          // add a class to this div: 'well'
+          siteData.addClass("newEntry");
+          // add an id to the well to mark which well it is
+          siteData.attr("id", "siteInfo" + i);
+          // append the well to the well section
+          $("#site-Data").append(siteData);
+
+          // Now add all of our character data to the well we just placed on the page
+
+          // make the name an h2,
+          $("#siteInfo" + i).append("<h1>Site:" + data[i].site + "</h1>");
+          // the role an h3,
+          $("#siteInfo" + i).append("<h1>Username:" + data[i].username + "</h1>");
+          // the age an h3,
+          $("#siteInfo" + i).append("<h1>Password:" + data[i].sPassword + "</h1>");
+        }
+      });
+    });
   }
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.name);
   });
-});
 
+});
